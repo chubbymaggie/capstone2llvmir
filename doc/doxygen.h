1a131ba03ad30a466a747e6b4463fa9dcb562f55
Peter Matula <peter.matula@avast.com>
2017-12-12 15:46:02 +0100

    initial commit

---
+++ doc/doxygen.h
@@ -0,0 +1,13 @@
+/**
+* @file doc/doxygen.h
+* @brief Documentation of the main page and the used namespaces.
+* @copyright (c) 2017 Avast Software, licensed under the MIT license
+*/
+
+/**
+@mainpage
+
+This is an automatically generated API documentation for the
+<a href="https://github.com/avast-tl/capstone2llvmir">Capstone2LlvmIR project</a>.
+
+*/
